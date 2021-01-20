29755654223fd5a6ebfe13a81c9abc98c0dece65
FixedC4ke <drmixe@gmail.com>
2021-01-20 16:25:39 +0500

    Добавлен объект снаряд + ActiveX объект для задания настроек (временно отключен, т.к. невозможно запустить проект, если на ПК нет Microsoft Office)

--- source/Main/Program.cs
+++ source/Main/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.Win32.SafeHandles;
 using System.IO.MemoryMappedFiles;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace SpaceInvaders
 {
@@ -13,16 +14,35 @@ namespace SpaceInvaders
         private static readonly Type ManagerT = Type.GetTypeFromProgID("SImanager.Manager");
         private static readonly Type ShipT = Type.GetTypeFromProgID("SIship.Ship");
         private static readonly Type CartT = Type.GetTypeFromProgID("SIcart.Cart");
+        private static readonly Type PatronT = Type.GetTypeFromProgID("SIpatron.Patron");
+        private static readonly Type SettingsT = Type.GetTypeFromProgID("SettingsActiveX.pdf_Reader");
+
         private static object manager;
         static void Main(string[] args)
         {
             manager = Activator.CreateInstance(ManagerT); //создание объекта-диспетчера
 
             object cart = Activator.CreateInstance(CartT); //создание объекта-тачанки
+
+
             int offset = (int)ManagerT.InvokeMember("Draw", System.Reflection.BindingFlags.InvokeMethod, null, manager, new object[] { "cart" }); //вывод тачанки на консоль
             CartT.GetProperty("Offset").SetValue(cart, offset);
 
             GenerateLineOfShipsOf(10);
+
+            //dynamic activeX = Activator.CreateInstance(SettingsT);
+            //Dictionary<string, string> check = (Dictionary<string, string>)SettingsT.InvokeMember("getSettings", System.Reflection.BindingFlags.InvokeMethod, null, activeX, null); //вывод тачанки на консоль
+            
+            //string value = "";
+            //if (check.TryGetValue("string1", out value))
+            //{
+            //    Console.WriteLine("For key = \"tif\", value = {0}.", value);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Key = \"tif\" is not found.");
+            //}
+            //GenerateLineOfShipsOf(Int32.Parse(value));
 
             while (true) //обработка нажатия клавиш
             {
@@ -36,6 +56,15 @@ namespace SpaceInvaders
                 else if (key.Key == ConsoleKey.LeftArrow)
                 {
                     CartT.InvokeMember("Move", System.Reflection.BindingFlags.InvokeMethod, null, cart, new object[] { (short)-2 });
+                }
+                else if (key.Key == ConsoleKey.Spacebar)
+                {
+                    object patron = Activator.CreateInstance(PatronT);
+
+                    int offset2 = (int)ManagerT.InvokeMember("Draw", System.Reflection.BindingFlags.InvokeMethod, null, manager, new object[] { "patron" }); //вывод тачанки на консоль
+                    PatronT.GetProperty("Offset").SetValue(patron, offset2);
+                    PatronT.InvokeMember("Action", System.Reflection.BindingFlags.InvokeMethod, null, patron, null);
+
                 }
             }
         }
---
+++ source/Sipatron/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIpatron
+{
+    public struct Entity
+    {
+        public short X;
+        public short Y;
+        public IntPtr TypeA;
+    }
+}
